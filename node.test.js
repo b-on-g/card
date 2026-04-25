2d45a84e42ea933231d9917d7c156e10926e182c
Dev-cmyser <Dev-cmyser@users.noreply.github.com>
2026-04-25 08:05:09 +0000

    Deploying to gh-pages from @ b-on-g/card@1b3188626d066e788d4a1e75e4e9786ea4038138 🚀

--- node.test.js
+++ node.test.js
@@ -2431,6 +2431,8 @@ var $;
         'field',
         'image',
         'spirit',
+        'hue',
+        'hue_spread',
     ]);
 })($ || ($ = {}));
 
@@ -3938,7 +3940,7 @@ var $;
                         }
                     }
                 }
-                else if (key === '@media') {
+                else if (key === '@media' || key === '@container') {
                     const media = config[key];
                     for (let query of Object.keys(media).reverse()) {
                         rules.push('}\n');
